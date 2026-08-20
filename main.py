dae503d2f6cf2a7b764876a2a95b0bcc2e811546
yogesh gavade <yogeshgawade798@gmail.com>
2026-08-20 16:46:03 +0530

    feat(api):added the path & Query parameter and the Request and Response

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from mockdata import products
+
 
 app=FastAPI()
 
@@ -17,3 +19,42 @@ def about():
     return { "message":"This is the about the page of the FastAPI application."}
 
 
+
+@app.get("/product")
+
+def get_product():
+    return products
+
+
+@app.get("/product/{product_id}")
+def get_product_by_id(product_id: int):
+    
+    for oneproduct in products:
+        if oneproduct.get("id")== product_id:
+            return oneproduct
+    return{
+         "message": "Product not found"
+    }
+    
+        
+
+@app.get("/greet")
+
+def greet(name: str):
+
+    return {
+        "message": f"helllo everyone My name is {name} !!!!!!! how are you doing"
+    }
+
+
+
+@app.get("/some")
+
+def some(request: Request):
+
+    print(request.query_params)
+
+
+    return {
+        "some":"this is the my age"
+    }
---
+++ mockdata.py
@@ -0,0 +1,20 @@
+products = [
+    {
+        "id": 1,
+        "title": "Learning FastAPI",
+        "description": "This is the FastAPI learning project for beginners.",
+        "price": 25
+    },
+    {
+        "id": 2,
+        "title": "FastAPI Best Practices",
+        "description": "Learn the best practices of FastAPI for building robust applications.",
+        "price": 30
+    },
+    {
+        "id": 3,
+        "title": "Advanced FastAPI",
+        "description": "Take your FastAPI skills to the next level with advanced concepts.",
+        "price": 40
+    }
+]
